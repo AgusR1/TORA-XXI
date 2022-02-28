9c451c4302f79080ba5e2d16e050ee34aefb20af
Agustin Rosales <50423482+AgusR1@users.noreply.github.com>
2022-02-28 11:35:21 -0300

    añadiendo referencia a inputs de la matriz

--- src/components/MenuProblema/menuProblema.jsx
+++ src/components/MenuProblema/menuProblema.jsx
@@ -1,28 +1,37 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useEffect,useRef } from "react";
 import useData from "../hooks/useData";
 import { BotonOpcion, CardTable, GridMenuProblema,GridSelect,Option,PanelBotones,SelectAlgoritmo,TitleProblem } from "./menuProblemaCommons";
 import TableProblema from "./TableProblema";
 
 const MenuProblema = () => {
     const data = useData();
+    const fuentesRef = useRef([]);
+    const destinosRef = useRef([]);
+    const demandasRef = useRef([]);
+    const ofertasRef = useRef([]);
+    const camposRef = useRef([]);
+    const algoritmoRef=useRef("");
+    const resolver=()=>{
+        console.log(fuentesRef.current[0]);
+    }
     return(
         <Fragment>
             <GridMenuProblema>
                 <TitleProblem>{data.nombreProblema}</TitleProblem>
                 <CardTable>
-                    <TableProblema data={data} />
+                    <TableProblema camposRef={camposRef} ofertasRef={ofertasRef} demandasRef={demandasRef} fuentesRef={fuentesRef} destinosRef={destinosRef} data={data} />
                 </CardTable>
                 <PanelBotones>
                     <GridSelect>
-                        <label>Seleccione el algoritmo</label>
-                        <SelectAlgoritmo id="selectAlgoritmo">
-                            <Option value="rutaMasCorta" >Ruta mas corta</Option>
+                        <label style={{fontFamily:"Abel"}} >Seleccione el algoritmo</label>
+                        <SelectAlgoritmo ref={algoritmoRef} id="selectAlgoritmo">
+                            <Option value="costoMinimo" >Costo minimo</Option>
                             <Option value="esquinaNoroeste">Esquina noroeste</Option>
                             <Option value="vogel">Vogel</Option>
                         </SelectAlgoritmo>
                     </GridSelect> 
                     <PanelBotones>
-                        <BotonOpcion>Resolver</BotonOpcion>
+                        <BotonOpcion onClick={resolver} whileHover={{ scale: 1.09 }}>Resolver</BotonOpcion>
                     </PanelBotones>
                 </PanelBotones>
             </GridMenuProblema>     
