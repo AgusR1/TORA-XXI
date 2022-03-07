4b9bd7bc6fba8f18428a5686e5d6d1987fd769d0
Agustin Rosales <50423482+AgusR1@users.noreply.github.com>
2022-03-07 19:45:44 -0300

    completando renderizado de resultados

--- src/components/MenuProblema/menuProblema.jsx
+++ src/components/MenuProblema/menuProblema.jsx
@@ -1,7 +1,8 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, {useEffect, Fragment, useState, useRef } from "react";
 import useData from "../hooks/useData";
-import { BotonOpcion, CardTable, GridMenuProblema, GridSelect, Option, PanelBotones, SelectAlgoritmo, TitleProblem } from "./menuProblemaCommons";
+import { BotonOpcion, BotonVolver, CardTable, GridSolucion, GridMenuProblema, GridSelect, GridDetalle, Option, PanelBotones, SelectAlgoritmo, TitleProblem, CuadroSolucion, TextSolucion, CuadroCosto, DivDetalle, CuadroNum } from "./menuProblemaCommons";
 import TableProblema from "./TableProblema";
+import { v4 as uuidv4 } from 'uuid';
 
 const MenuProblema = () => {
     const data = useData();
@@ -11,17 +12,23 @@ const MenuProblema = () => {
     const ofertasRef = useRef([]);
     const camposRef = useRef([]);
     const algoritmoRef = useRef("");
+    const [minCostos,setMinCostos]=useState([]);
+    const [listaFuentes,setListaFuentes]=useState([]);
+    const [listaOfertas,setListaOfertas]=useState([]);
+    const [listaDemandas,setListaDemandas]=useState([]);
+    const [listaClientes,setListaClientes]=useState([]);
+    const [costoTotal,setCostoTotal]=useState(0);
+    const [pantalla, setPantalla] = useState("tabla");
     const resolver = () => {
         switch (algoritmoRef.current.value) {
-            case "costoMinimo":
+            case "Costo minimo":
                 let sumaDemanda = 0;
                 let sumaOferta = 0;
                 let matrizCostes = [];
                 let tablaDemandas = [];
                 let tablaOfertas = [];
-                let minCosto;
                 let costoTotal = 0;
-                let diferencia=0;
+                let diferencia = 0;
                 for (let i = 0; i < demandasRef.current.length; i++) {
                     //paso los valores del input a un array donde manipularlos
                     tablaDemandas[i] = parseFloat(demandasRef.current[i].value, 2);
@@ -39,24 +46,25 @@ const MenuProblema = () => {
                     matrizCostes[i] = parseFloat(camposRef.current[i].value, 2);
                 }
                 //balanceo
-                if(sumaOferta>sumaDemanda){
-                    diferencia=sumaOferta-sumaDemanda;//calculamos la diferencia entre oferta y demanda
-                    sumaDemanda=sumaDemanda+diferencia;
+                if (sumaOferta > sumaDemanda) {
+                    diferencia = sumaOferta - sumaDemanda;//calculamos la diferencia entre oferta y demanda
+                    sumaDemanda = sumaDemanda + diferencia;
                     tablaDemandas.push(diferencia);//añadimos la diferencia como la demanda del nuevo destino ficticio
                     let indices = [];
-                    let count=0;
+                    let count = 0;
                     for (let i = 0; i < matrizCostes.length; i++) {
-                        if((i+1)%(tablaDemandas.length-1)===0){
-                            indices.push(i+count);
+                        if ((i + 1) % (tablaDemandas.length - 1) === 0) {
+                            indices.push(i + count);
                             count++;
                         }
                     }
                     for (let i = 0; i < indices.length; i++) {
-                        matrizCostes.splice((indices[i]+1), 0, 0);
+                        matrizCostes.splice((indices[i] + 1), 0, 0);
                     }
                 }
                 //el bucle se ejecutara hasta el punto en el que la demanda queda satisfecha
                 do {
+                    let minCosto;
                     let pos;
                     let auxPos;
                     let demanda;
@@ -74,17 +82,22 @@ const MenuProblema = () => {
                         case 0:
                             demanda = tablaDemandas[tablaDemandas.length - 1];
                             posOferta = Math.ceil((pos + 1) / tablaDemandas.length);//para determinar que oferta disponible tiene este cliente con este costo asociado necesito realizar la siguiente operacion matematica que me devuelva el numero de fila al que corresponde ej pos=1 y length=3 => 1/3 redondeado hacia arriba me devuelve 1 o pos=4 7 length=3 => 4/3 round up es 2
-                            oferta = tablaOfertas[posOferta-1];
+                            oferta = tablaOfertas[posOferta - 1];
                             //ya tenemos el costo minimo, la oferta y la demanda correspondiente para dicho costo, ahora se asignan los recursos
-                            if (oferta > 0 && demanda>0){
+                            if (oferta > 0 && demanda > 0) {
                                 if (oferta > demanda) {
                                     //utilizamos la longitud del array de demandas como posicion porque sabemos que si el case es 0 entonces es el ultimo elemento del array de demanda
                                     tablaDemandas[tablaDemandas.length - 1] = 0;//si la oferta supera a la demanda entonces la demanda de ese cliente queda satisfecha y se setea en 0
-                                    tablaOfertas[posOferta-1] = oferta - demanda;//restamos aquello que fue utilizado para suplir la demanda anterior al inventario del deposito
+                                    tablaOfertas[posOferta - 1] = oferta - demanda;//restamos aquello que fue utilizado para suplir la demanda anterior al inventario del deposito
                                     costoTotal = costoTotal + demanda * matrizCostes[pos];//calculamos el costo total, sabemos que se cubrio toda la demanda restante por lo que el calculo es demanda*costo 
                                     sumaDemanda = sumaDemanda - demanda;//restamos la demanda que ya se suplio
                                     sumaOferta = sumaOferta - demanda;//restamos la oferta que tenemos disponible
                                     matrizCostes[pos] = Math.max(...matrizCostes) + 1;//hacemos que el actual costo minimo tome un valor mayor al maximo coste, de este modo nos aseguramos que la funcion min no vuelva a usarlo
+                                    setMinCostos(cost => [...cost, minCosto]);
+                                    setListaOfertas(listaO => [...listaO, demanda]);
+                                    setListaFuentes(listaF => [...listaF, fuentesRef.current[posOferta - 1].value]);
+                                    setListaDemandas(listaD => [...listaD, demanda]);
+                                    setListaClientes(listaC => [...listaC, destinosRef.current[tablaDemandas.length - 1].value]);
                                 } else {
                                     tablaDemandas[tablaDemandas.length - 1] = demanda - oferta;//si la demanda es igual o superior a la oferta restar la oferta nos dejara con la demanda restante por satisfacer o con la demanda en 0
                                     tablaOfertas[posOferta - 1] = 0;//seteamos en 0 el valor de la oferta porque todo lo que estaba disponible fue entregado
@@ -92,17 +105,22 @@ const MenuProblema = () => {
                                     sumaDemanda = sumaDemanda - oferta;//se calcula la demanda total restante en base a lo que la oferta entrego
                                     sumaOferta = sumaOferta - oferta;
                                     matrizCostes[pos] = Math.max(...matrizCostes) + 1;//hacemos que el actual costo minimo tome un valor mayor al maximo coste, de este modo nos aseguramos que la funcion min no vuelva a usarlo
+                                    setMinCostos(cost => [...cost, minCosto]);
+                                    setListaOfertas(listaO => [...listaO, oferta]);
+                                    setListaFuentes(listaF => [...listaF, fuentesRef.current[posOferta - 1].value]);
+                                    setListaDemandas(listaD => [...listaD, demanda]);
+                                    setListaClientes(listaC => [...listaC, destinosRef.current[tablaDemandas.length - 1].value]);
                                 }
-                            }else{
+                            } else {
                                 matrizCostes[pos] = Math.max(...matrizCostes) + 1;//aquellos campos que se encuentren en una fila o columna donde la demanda fue satisfecha o la oferta es 0 se les asigna un valor por sobre el maximo, de este modo garantizamos que la funcion Math.min() no vuelva a buscarlo
                             }
                             break;
                         default:
                             demanda = tablaDemandas[auxPos - 1]//auxPos siempre vale +1 la posicion correspondiente en el array de demanda
                             posOferta = Math.ceil((pos + 1) / tablaDemandas.length);//para determinar que oferta disponible tiene este cliente con este costo asociado necesito realizar la siguiente operacion matematica que me devuelva el numero de fila al que corresponde ej pos=1 y length=3 => 1/3 redondeado hacia arriba me devuelve 1 o pos=4 7 length=3 => 4/3 round up es 2
-                            oferta = tablaOfertas[posOferta-1];
+                            oferta = tablaOfertas[posOferta - 1];
                             //ya tenemos el costo minimo, la oferta y la demanda correspondiente para dicho costo, ahora se asignan los recursos
-                            if (oferta > 0 && demanda>0) {
+                            if (oferta > 0 && demanda > 0) {
                                 if (oferta > demanda) {
                                     tablaDemandas[auxPos - 1] = 0;//si la oferta supera a la demanda entonces la demanda de ese cliente queda satisfecha y se setea en 0
                                     tablaOfertas[posOferta - 1] = oferta - demanda;//restamos aquello que fue utilizado para suplir la demanda anterior al inventario del deposito
@@ -110,26 +128,37 @@ const MenuProblema = () => {
                                     sumaDemanda = sumaDemanda - demanda;//restamos la demanda que ya se suplio
                                     sumaOferta = sumaOferta - demanda;//restamos la oferta que tenemos disponible
                                     matrizCostes[pos] = Math.max(...matrizCostes) + 1;//hacemos que el actual costo minimo tome un valor mayor al maximo coste, de este modo nos aseguramos que la funcion min no vuelva a usarlo
-                                } else {                          
+                                    setMinCostos(cost => [...cost, minCosto]);
+                                    setListaOfertas(listaO => [...listaO, demanda]);
+                                    setListaFuentes(listaF => [...listaF, fuentesRef.current[posOferta - 1].value]);
+                                    setListaDemandas(listaD => [...listaD, demanda]);
+                                    setListaClientes(listaC => [...listaC, destinosRef.current[auxPos - 1].value]);
+                                } else {
                                     tablaDemandas[auxPos - 1] = demanda - oferta; //si la demanda es igual o superior a la oferta restar la oferta nos dejara con la demanda restante por satisfacer o con la demanda en 0
                                     tablaOfertas[posOferta - 1] = 0;//seteamos en 0 el valor de la oferta porque todo lo que estaba disponible fue entregado
                                     costoTotal = costoTotal + oferta * matrizCostes[pos];//el costo total se calcula en base a cuanto se entrego en lugar de cuanto se demando
                                     sumaDemanda = sumaDemanda - oferta;//se calcula la demanda total restante en base a lo que la oferta entrego
                                     sumaOferta = sumaOferta - oferta;
                                     matrizCostes[pos] = Math.max(...matrizCostes) + 1;//hacemos que el actual costo minimo tome un valor mayor al maximo coste, de este modo nos aseguramos que la funcion min no vuelva a usarlo
+                                    setMinCostos(cost => [...cost,minCosto]);
+                                    setListaOfertas(listaO => [...listaO,oferta]);
+                                    setListaFuentes(listaF => [...listaF, fuentesRef.current[posOferta-1].value]);
+                                    setListaDemandas(listaD=>[...listaD,demanda]);
+                                    setListaClientes(listaC => [...listaC, destinosRef.current[auxPos-1].value] );
                                 }
-                            }else{
+                            } else {
                                 matrizCostes[pos] = Math.max(...matrizCostes) + 1;////aquellos campos que se encuentren en una fila o columna donde la demanda fue satisfecha o la oferta es 0 se les asigna un valor por sobre el maximo, de este modo garantizamos que la funcion Math.min() no vuelva a buscarlo
                             }
                             break;
                     }
-                } while (sumaDemanda>0);
-                console.log(costoTotal);
+                } while (sumaDemanda > 0);
+                setCostoTotal(costoTotal);
+                setPantalla("solucion");
                 break;
-            case "esquinaNoroeste":
+            case "Esquina noroeste":
 
                 break;
-            case "vogel":
+            case "Vogel":
 
                 break;
             default:
@@ -140,25 +169,53 @@ const MenuProblema = () => {
     }
     return (
         <Fragment>
-            <GridMenuProblema>
-                <TitleProblem>{data.nombreProblema}</TitleProblem>
-                <CardTable>
-                    <TableProblema camposRef={camposRef} ofertasRef={ofertasRef} demandasRef={demandasRef} fuentesRef={fuentesRef} destinosRef={destinosRef} data={data} />
-                </CardTable>
-                <PanelBotones>
-                    <GridSelect>
-                        <label style={{ fontFamily: "Abel" }} >Seleccione el algoritmo</label>
-                        <SelectAlgoritmo ref={algoritmoRef} id="selectAlgoritmo">
-                            <Option value="costoMinimo" >Costo minimo</Option>
-                            <Option value="esquinaNoroeste">Esquina noroeste</Option>
-                            <Option value="vogel">Vogel</Option>
-                        </SelectAlgoritmo>
-                    </GridSelect>
+            {pantalla === "tabla" &&
+                < GridMenuProblema >
+                    <TitleProblem>{data.nombreProblema}</TitleProblem>
+                    <CardTable>
+                        <TableProblema camposRef={camposRef} ofertasRef={ofertasRef} demandasRef={demandasRef} fuentesRef={fuentesRef} destinosRef={destinosRef} data={data} />
+                    </CardTable>
                     <PanelBotones>
-                        <BotonOpcion onClick={resolver} whileHover={{ scale: 1.09 }}>Resolver</BotonOpcion>
+                        <GridSelect>
+                            <label style={{ fontFamily: "Abel" }} >Seleccione el algoritmo</label>
+                            <SelectAlgoritmo ref={algoritmoRef} id="selectAlgoritmo">
+                                <Option value="Costo minimo" >Costo minimo</Option>
+                                <Option value="Esquina noroeste">Esquina noroeste</Option>
+                                <Option value="Vogel">Vogel</Option>
+                            </SelectAlgoritmo>
+                        </GridSelect>
+                        <PanelBotones>
+                            <BotonOpcion onClick={resolver} whileHover={{ scale: 1.09 }}>Resolver</BotonOpcion>
+                            <BotonVolver whileHover={{ scale: 1.09 }}>Volver</BotonVolver>
+                        </PanelBotones>
                     </PanelBotones>
-                </PanelBotones>
-            </GridMenuProblema>
+                </GridMenuProblema>
+            }
+            {pantalla==="solucion" &&
+                <GridSolucion>
+                    <TitleProblem>{algoritmoRef.current.value}</TitleProblem>
+                    <CuadroSolucion initial={{ x: 2000 }} animate={{ x: 0, type: 'tween' }}>
+                        <GridDetalle>
+                            {
+                                minCostos.map((costo,index) => {
+                                    return (
+                                        <DivDetalle>
+                                            <CuadroNum><TextSolucion>#{index + 1}</TextSolucion></CuadroNum>
+                                            <TextSolucion key={uuidv4()}>El deposito <b>{listaFuentes[index]}</b> debe entregar <b>{listaOfertas[index]}</b>  insumos al cliente <b>{listaClientes[index]}</b> que tiene una demanda de <b>{listaDemandas[index]}</b>  a un costo de <b>${costo}</b> 
+                                            </TextSolucion>
+                                        </DivDetalle>
+                                    );
+                                })
+                            }
+                            <CuadroCosto>
+                                <TextSolucion>
+                                    El costo total: <b>${costoTotal}</b>
+                                </TextSolucion>
+                            </CuadroCosto>
+                        </GridDetalle>
+                    </CuadroSolucion>
+                </GridSolucion>
+            }
         </Fragment>
     );
 }
